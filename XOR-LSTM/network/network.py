556111da6c108452ab86a55c0734518479d299fb
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2020-08-20 16:53:17 -0700

    Error
    Can't get network to go below 0.7 loss. Must be somthing wrong with tensor maniplulations!

--- XOR-LSTM/network/network.py
+++ XOR-LSTM/network/network.py
@@ -1,6 +1,7 @@
 import torch
 from tqdm import tqdm
 import numpy as np
+from copy import copy
 
 class Network(torch.nn.Module):
 
@@ -10,7 +11,7 @@ class Network(torch.nn.Module):
 
         self.define_network(in_dim, out_dim, hid_dim, num_lay)
 
-        self.loss = torch.nn.BCELoss()
+        self.loss = torch.nn.BCEWithLogitsLoss()
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
 
         if torch.cuda.is_available():
@@ -28,7 +29,7 @@ class Network(torch.nn.Module):
             hidden_size=hid_dim,
             num_layers=num_lay)
 
-        self.fc = torch.nn.Linear(1, 1)
+        self.fc = torch.nn.Linear(hid_dim, 1)
 
         self.sigmoid = torch.nn.Sigmoid()
         self.leaky_relu = torch.nn.LeakyReLU()
@@ -44,8 +45,13 @@ class Network(torch.nn.Module):
 
         #print(out[0][-1].shape)
 
-        out = self.fc(out[0][-1].reshape(-1, 1))
+        #print(out[0].shape)
+
+
+        out = self.fc(out[0][0].reshape(x.shape[1], -1))
         out = self.sigmoid(out)
+
+        #out = self.sigmoid(out[0][-1].reshape(-1, 1))
 
         #print(out.shape)
 
@@ -53,36 +59,54 @@ class Network(torch.nn.Module):
 
     def optimize(self, x, y, batch_sz, iters):
         
+        a = torch.Tensor(x)
+        a = 1-torch.sum(a, dim=1, keepdim=True)%2
+        print(a.shape)
+        print()
+
+        #print(a[a%2 == 0])
+        print(a)
+        print("ACC: ", self.accuracy(a, torch.Tensor(y)))
+
         x = torch.Tensor(x.reshape([x.shape[1], x.shape[0], 1]))
         y = torch.Tensor(y)
 
-        print(x.shape)
-        print(y.shape)
+        print(x)
+        print(y)
 
         num_batches = y.shape[0]//batch_sz
 
         for i in tqdm(range(iters), "Training Network"):
-        
+            
+            run_loss = 0
+            run_acc = 0
+
             for b in range(num_batches):
                 # zero the parameter gradients
                 self.optimizer.zero_grad()
                 # forward + backward + optimize
                 p = self.forward(x[:,b*batch_sz:(b+1)*batch_sz,:])
-                self.accuracy(p, y[b*batch_sz:(b+1)*batch_sz, :])
+                #self.accuracy(p, y[b*batch_sz:(b+1)*batch_sz, :])
                 loss = self.loss(p, y[b*batch_sz:(b+1)*batch_sz, :])
-                self.historical_loss.append(loss.detach())
+
+                run_loss += loss.detach()/num_batches
+                run_acc += self.accuracy(p, y[b*batch_sz:(b+1)*batch_sz, :])/num_batches
+
                 loss.backward()
                 self.optimizer.step()
     
+            self.historical_loss.append(copy(run_loss))  
+            self.historical_accuracy.append(run_acc) 
+
+        x = 1-torch.sum(x.reshape(y.shape[0], -1), dim=1, keepdim=True)%2
+        print(x.shape)
+        print(self.accuracy(x, y))
 
     def accuracy(self, p, y):
 
-        c = (p.clone().detach()>0.5).float()
+        accuracy = ((p > 0.5) == (y > 0.5)).type(torch.FloatTensor).mean()
 
-        a = torch.abs(c-y.clone().detach())
-
-        self.historical_accuracy.append(1-torch.sum(a).item()/y.shape[0])
-
+        return accuracy
 
 def main():
 
