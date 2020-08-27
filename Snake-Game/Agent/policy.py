4e655811dca8f60181455e0ccf8811c2ee1d94a4
Gregory Eales <gregoeales@aol.com>
2020-08-27 11:29:44 -0700

    Update

--- Snake-Game/Agent/policy.py
+++ Snake-Game/Agent/policy.py
@@ -6,19 +6,58 @@ from tqdm import tqdm
 
 class Policy(torch.nn.Module):
 
-	def __init__(self, in_dim, hidd_dim, out_dim, alpha=1e-3):
+	def __init__(self, in_channel, hidd_channel, kernal_sz, lin_dim, alpha=1e-3):
 
+		super(Policy, self).__init__()
+
+		self.in_channel = in_channel
+		self.hidd_channel = hidd_channel
+		self.kernal_sz = kernal_sz
+		self.lin_dim = lin_dim
+
+		self.conv1 = torch.nn.Conv2d(
+			in_channels=in_channel,
+			out_channels=hidd_channel,
+			kernel_size=kernal_sz
+			)
+
+		self.conv2 = torch.nn.Conv2d(
+			in_channels=hidd_channel,
+			out_channels=hidd_channel,
+			kernel_size=kernal_sz
+			)
+
+		self.conv3 = torch.nn.Conv2d(
+			in_channels=hidd_channel,
+			out_channels=hidd_channel,
+			kernel_size=kernal_sz
+			)
+
+		self.l1 = torch.nn.Linear(lin_dim, 4)
+
+		self.optimizer = torch.optim.Adam(self.parameters(), lr=alpha)
+
+		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
+		self.to(self.device)
+
+
+	def loss(self, pi, pi_k, epsilon, advantage):
+
+		term1 = pi/pi_k * advantage
+		term2 = torch.clamp(advantage, min=1-epsilon, max=1+epsilon)
+
+		return torch.sum(torch.min(term1, term2))
+
+
+	def forward(self, x):
+
+		out = torch.Tensor(x).to(self.device)
 		
-		self.in_dim = in_dim
-		self.hidd_dim = hidd_dim
-		self.out_dim = out_dim
+		out = self.conv1(out)
+		out = self.conv2(out)
+		out = self.conv2(out)
 
-		self.optimizer = torch.optim.Adam(self.parameters(), alpha=alpha)
-
-
-	def forward(self):
-
-		pass
+		return out.to(torch.device('cpu:0'))
 
 
 	def optimize(self, x, y, epochs=10, batch_sz=16):
@@ -31,5 +70,19 @@ class Policy(torch.nn.Module):
 			for b in range(num_batch):
 				
 				p = self.forward(x[b*batch_sz:(b+1)*(batch_sz)])
+
+
+def main():
+	policy = Policy(in_channel=1, hidd_channel=16, kernal_sz=2, lin_dim=10)
+
+	x = torch.randn(1, 1, 16, 16)
+
+	p = policy(x)
+
+	print(p.shape)
+
+
+if __name__ == "__main__":
+	main()
 
 
--- Snake-Game/train.py
+++ Snake-Game/train.py
@@ -8,18 +8,18 @@ from agent.ppo import PPO
 
 def train(env, agent, episodes, steps):
 
-
 	for e in range(episodes):
 
-		prev_state = env.reset()
+		state = env.reset()
 
 		for i in range(steps):
 
 			action = agent.act(state)
-			state, reward, done, info = env.step(action)
-			agent.store(state, prev_state, action, reward, done)
 			prev_state = state
+			state, reward, done, info = env.step(action)
 
+			agent.store(state, prev_state, action, reward, done)
+			
 			if done:
 				break
 
@@ -31,4 +31,7 @@ def main():
 	env = gym.make('gym_snake:snake-v0')
 	agent = PPO()
 	train(env, agent, episodes=10, steps=200)
+
+if __name__ == "__main__":
+	main()
 
